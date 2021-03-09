f4017bbfae859ae7e6c669156de0173af01fd2f9
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2021-03-09 21:47:16 +0100

    build: 📦️ remove plugin process

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 exports.onCreateWebpackConfig = ({ actions, plugins }) => {
     actions.setWebpackConfig({
         // eslint-disable-next-line new-cap
-        plugins: [new plugins.provide({ process: 'process/browser' })],
+        // plugins: [new plugins.provide({ process: 'process/browser' })],
         resolve: {
             alias: {
                 path: require.resolve('path-browserify'),
