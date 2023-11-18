8f30374474072a36bd8ad19f5bee7ae050871ebe
Jimmy <jimmy.beldone@gmail.com>
2023-11-18 19:56:30 +0100

    fix: 🐛 stylelint config and rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,13 +2,17 @@ module.exports = {
     defaultSeverity: 'warning',
     extends: [
         // add more generic rulesets here, such as:
-        // "stylelint-config-standard",
-        'stylelint-prettier/recommended',
+        'stylelint-config-standard',
+        // 'stylelint-prettier/recommended',
         'stylelint-stylus/standard',
     ],
-    // fix: true,
-    plugins: ['stylelint-stylus', 'stylelint-prettier'],
+    fix: true,
+    plugins: [
+        'stylelint-stylus',
+        // 'stylelint-prettier'
+    ],
     rules: {
+        indentation: 4,
         // override/add rules settings here, such as:
         'stylus/declaration-colon': 'never',
         'stylus/semicolon': 'never',
